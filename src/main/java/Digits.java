e8a40a745a280df382ef6f66bbf4ddc7b5ed572a
TrevorAPCS <142834649+TrevorAPCS@users.noreply.github.com>
2024-04-15 11:15:34 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,12 +7,23 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+	    String str = num + "";
+	    digitList = new ArrayList<Integer>();
+	    for(int i = 0; i < str.size(); i++){
+		digitList.add(Integer.parseInt(str.substring(i, i+1)));
+	    }
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
+		int prev = -1;
+		for(int i = 0; i < digitList.size(); i++){
+			if(digitList.get(i) <= prev){
+				return false;
+			}
+			prev = digitList.get(i);
+		}
+		return true;
 
 	}
 	
